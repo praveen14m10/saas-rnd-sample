31d2201a55ea4f78ff83d07e0cf11f8714c57947
praveen14m10 <praveen@inesssolutions.com.com>
2024-08-05 16:03:15 +0530

    Hello orld msg

---
+++ src/cfehome/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request,*args,**kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title":my_title
+    }
+    html_template = "home.html"
+    return render(request,html_template, my_context)
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(**my_context) # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
